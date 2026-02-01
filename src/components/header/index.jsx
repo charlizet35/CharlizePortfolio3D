57a15a84d26ad6b9ec87042e2a322d4a8a5d483a
charlizet35 <charlizet35@gmail.com>
2026-01-31 18:51:43 -0600

    fixing up formattig of navbar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,51 +1,33 @@
-import React , { useState } from 'react'
-import { ReactComponent as MenuIcon } from '../assets/HomePage2.svg';
-
+import React from 'react';
+import MenuIcon from '../../assets/HomePage2.svg';
+import AboutMeIcon from '../../assets/AboutMe.svg';
+import ExperienceIcon from '../../assets/Experience.svg';
+import ProjectsIcon from '../../assets/Projects.svg';
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    
-    const buttonClasses = 'text-gray-100 font-bold-text-sm px-2 py-1 border-2 border-gray-100 hover:bg-gray-100 hover:text-gray-800 rounded-lg transition duration-300';
-
-    const buttons = (
-        <>
-            <button className={buttonClasses}>About Me</button>
-            <button className={buttonClasses}>Experience</button>
-            <button className={buttonClasses}>Projects</button>
-        </>
-    )
   return (
-    <nav className='bg-gray-800 text-white fixed w-full z-10 top-0 shadow'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-            <div className='flex items-center justify-between h-16'>
-                <div className='flex flex-row w-full justify-between'>
-                    <div className='text-xl font-bold'>
-                        Charlize
-                    </div>
-                    <div className='hidden md: block'>
-                        <div className='flex ml-10 items-baseline space-x-2'>
-                            {buttons}
-                        </div>
-                    </div>
-                </div>
-                <div className='md:hidden'>
-                    <button onClick={()=>{setIsOpen(!isOpen)}} type='button'className='fill-gray-100'>
-                        <svg>
-                            <MenuIcon className='w-6 h-6' /> {/* adjust size with Tailwind classes */}
-                        </svg>
-                    </button>
-                </div>
-            </div>
-        </div>
-        {
-            isOpen && (
-                <div className='flex flex-col gap-y-2 md:hidden px-4 sm:px-6 pb-2'>
-                    {buttons}
-                </div>
-            )
-        }
-    </nav>
-  )
-}
+    <nav className="relative w-full p-4 bg-gray-800 text-white fixed top-0 z-10 flex items-center">
+      {/* Left-aligned Home button with spacing */}
+      <div className="ml-4">
+        <button>
+          <img src={MenuIcon} alt="Home" className="w-4 h-4" />
+        </button>
+      </div>
 
-export default Header
+      {/* Centered icons with equal spacing */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-8">
+        <button>
+          <img src={AboutMeIcon} alt="About Me" className="w-4 h-4" />
+        </button>
+        <button>
+          <img src={ExperienceIcon} alt="Experience" className="w-4 h-4" />
+        </button>
+        <button>
+          <img src={ProjectsIcon} alt="Projects" className="w-4 h-4" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
