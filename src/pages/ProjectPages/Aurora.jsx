1e0058ddfaf22fb6bbf359975c6a82f84efef0b7
charlizet35 <charlizet35@gmail.com>
2026-07-09 14:05:42 -0500

    updating changes

--- src/pages/ProjectPages/Aurora.jsx
+++ src/pages/ProjectPages/Aurora.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import "../../App.css";
 import final_render from '../../assets/final.jpg';
-import demoReel from '../../assets/Demo Reel 2026.mov';
+import aurora1 from '../../assets/aurora_wip.png';
+import aurora2 from '../../assets/aurora_wip2.png';
+//import aurora4 from '../../assets/aurora_wip_4.png';
 import { useEffect, useContext} from 'react';
 import { SceneContext } from '../../App';
 
@@ -55,20 +57,36 @@ const Aurora = () => {
           <h3>02 Process</h3>
 
           <p>
-            I built this tool using Python 
-
+            I was inspired to create this tool after my friend requested for me to make a 3D scene to use for his song on Spotify Canvas. After listening to his song, the energy and tone I felt reflected that of space, stars, and aurora borealis'. I decided to create this tool to generate aurora borealis effects that could be used in his scene, and potentially by other artists in the future. 
           </p>
+
+          <p>
+            I tried to plan this project using an informal version of the Software Development Life Cycle. As both the client (artist) and developer in this scenario, I made a requirement analysis for the parameters based on how I envisioned an artist would want to use the tool. For the design phase, I defined the UI and tech stacks for the project, in which I used Python and the OpenMaya API for the core logic, and Qt.py for the UI. 
+          </p>
+
+          <p>
+            During my development phase, I created a mesh ribbon along the input curve, then developed a procedural shader network for that ribbon. I had many difficulties with the shader network, as I had to create a custom animation for the vertical streaks in the aurora, which involved testing many combinations of Maya's built-in noise and ramp nodes.
+          </p>
+
+          <p style={{ color: '#858585' }}>
+            Implementation and testing:
+          </p>
+
+
         </section>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '35px', paddingLeft: '45px', paddingRight: '45px', marginTop: '40px' }}>
-            <figure style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', margin: 0 }}>
-            <video 
-                src={demoReel} 
-                controls
-                style={{ width: '100%', borderRadius: '12px', display: 'block' }}
-            />
-            <figcaption className="caption">Demo Reel 2026</figcaption>
-            </figure>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '35px', paddingLeft: '45px', paddingRight: '45px', marginTop: '20px' }}>
+          <figure style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '40%', margin: 0 }}>
+            <img src={aurora1} alt="aurora_wip1" style={{ width: '200%', borderRadius: '12px', display: 'block' }} />
+            <figcaption className="caption"></figcaption>
+          </figure>
         </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '35px', paddingLeft: '45px', paddingRight: '45px', marginTop: '20px' }}>
+          <figure style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '40%', margin: 0 }}>
+            <img src={aurora2} alt="aurora_wip1" style={{ width: '200%', borderRadius: '12px', display: 'block' }} />
+            <figcaption className="caption"></figcaption>
+          </figure>
+        </div>
+
         
 
       </article>
